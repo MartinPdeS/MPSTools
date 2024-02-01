63d4c31f0570d98a378344a8de1032679c41d36d
MartinPdeS <Martin.Poinsinet.de.Sivry@gmail.com>
2024-02-01 14:03:02 -0800

    update on coordinate [publish]

--- MPSTools/tools/coordinates.py
+++ MPSTools/tools/coordinates.py
@@ -1,6 +1,11 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
 
 import numpy
 from dataclasses import dataclass
+from typing import Self
+
+from MPSPlots import render2D, render3D
 
 
 @dataclass
@@ -31,14 +36,37 @@ class CartesianCoordinates:
     z: numpy.ndarray
     """ Must be 1D-vector """
 
+    is_structured: bool = False
+    is_3D: bool = False
+
+    @property
+    def min_x(self) -> float:
+        return numpy.min(self.x_bounds)
+
+    @property
+    def max_x(self) -> float:
+        return numpy.max(self.x_bounds)
+
+    @property
+    def min_y(self) -> float:
+        return numpy.min(self.y_bounds)
+
+    @property
+    def max_y(self) -> float:
+        return numpy.max(self.y_bounds)
+
     @classmethod
-    def generate_from_boundaries(cls,
+    def generate_from_boundaries(
+            cls,
             x_limits: list = [-1, 1],
             y_limits: list = [-1, 1],
             z_limits: list = [-1, 1],
             x_points: int = 100,
             y_points: int = 100,
             z_points: int = 100):
+
+        cls.is_structured = True
+        cls.is_3D = True
 
         x = numpy.linspace(x_limits[0], x_limits[1], x_points)
         y = numpy.linspace(y_limits[0], y_limits[1], y_points)
@@ -50,21 +78,45 @@ class CartesianCoordinates:
             z=z
         )
 
+        instance.is_structured = True
+        instance.is_3D = True
+
         return instance
 
     @classmethod
-    def generate_from_cube(cls, size: float, center: tuple = (0, 0, 0), n_points: int = 100):
+    def generate_from_cube(
+            cls,
+            length: float,
+            center: tuple = (0, 0, 0),
+            n_points: int = 100):
+        """
+        Construct the coordinate system from a structured cube structure.
+
+        :param      cls:       The cls
+        :type       cls:       CartesianCoordinates
+        :param      length:    The length of the side of the square
+        :type       length:    float
+        :param      center:    The center
+        :type       center:    tuple
+        :param      n_points:  The number of points for each dimension [x, y, z]
+        :type       n_points:  int
+        """
         x0, y0, z0 = center
 
-        x = numpy.linspace(-size / 2, size / 2, n_points) + x0
-        y = numpy.linspace(-size / 2, size / 2, n_points) + y0
-        z = numpy.linspace(-size / 2, size / 2, n_points) + z0
+        x = numpy.linspace(-length / 2, length / 2, n_points) + x0
+        y = numpy.linspace(-length / 2, length / 2, n_points) + y0
+        z = numpy.linspace(-length / 2, length / 2, n_points) + z0
+
+        x_mesh, y_mesh, z_mesh = numpy.meshgrid(x, y, z)
 
         instance = CartesianCoordinates(
-            x=x,
-            y=y,
-            z=z
+            x=x_mesh,
+            y=y_mesh,
+            z=z_mesh
         )
+
+        instance.is_structured = True
+        instance.is_3D = True
 
         instance.dx = abs(x[1] - x[0])
         instance.dy = abs(y[1] - y[0])
@@ -73,11 +125,27 @@ class CartesianCoordinates:
         return instance
 
     @classmethod
-    def generate_from_square(cls, size: float, center: tuple = (0, 0), n_points: int = 100):
+    def generate_from_square(
+            cls,
+            length: float,
+            center: tuple = (0, 0),
+            n_points: int = 100):
+        """
+        Construct the coordinate system from a structured square structure.
+
+        :param      cls:       The cls
+        :type       cls:       CartesianCoordinates
+        :param      length:    The length of the side of the square
+        :type       length:    float
+        :param      center:    The center
+        :type       center:    tuple
+        :param      n_points:  The number of points for each dimension [x, y]
+        :type       n_points:  int
+        """
         x0, y0 = center
 
-        x = numpy.linspace(-size / 2, size / 2, n_points) + x0
-        y = numpy.linspace(-size / 2, size / 2, n_points) + y0
+        x = numpy.linspace(-length / 2, length / 2, n_points) + x0
+        y = numpy.linspace(-length / 2, length / 2, n_points) + y0
 
         x_mesh, y_mesh = numpy.meshgrid(y, x)
 
@@ -86,6 +154,9 @@ class CartesianCoordinates:
             y=y_mesh.T,
             z=0
         )
+
+        instance.is_structured = True
+        instance.is_3D = False
 
         instance.dx = abs(x[1] - x[0])
         instance.dy = abs(y[1] - y[0])
@@ -105,6 +176,93 @@ class CartesianCoordinates:
 
     def to_cartesian(self):
         return self
+
+    def shift_coordinates(self, shift: tuple) -> Self:
+        """
+        Shift the coordinates with a certain values
+
+        :param      shift:  The shift [x], [x, y] or [x, y, z]
+        :type       shift:  tuple
+
+        :returns:   The self instance
+        :rtype:     Self
+        """
+        coordinates = (self.x, self.y, self.z)
+        for coordinate, element in zip(coordinates, shift):
+            coordinate += element
+
+        return self
+
+    def scale_coordinates(self, scale: tuple) -> Self:
+        """
+        Scale the coordinates with a certain factor
+
+        :param      scale:  The shift [x], [x, y] or [x, y, z]
+        :type       scale:  tuple
+
+        :returns:   The self instance
+        :rtype:     Self
+        """
+        coordinates = (self.x, self.y, self.z)
+        for coordinate, element in zip(coordinates, scale):
+            coordinate *= element
+
+        return self
+
+    def plot(self) -> render2D.SceneList | render3D.SceneList:
+        """
+        Plots the coordinate structure
+
+        :returns:   The scene instance
+        :rtype:     render2D.SceneList | render3D.SceneList
+        """
+        if self.is_3D is True:
+            return self.plot_3D()
+        else:
+            return self.plot_2D()
+
+    def plot_2D(self) -> render2D.SceneList:
+        """
+        Plots the coordinate structure if it's a 2D structure
+
+        :returns:   The scene instance
+        :rtype:     render2D.SceneList
+        """
+        scene = render2D.SceneList()
+
+        ax = scene.append_ax(aspect_ratio='equal')
+
+        ax.add_scatter(x=self.x, y=self.y)
+
+        return scene
+
+    def plot_3D(self) -> render3D.SceneList:
+        """
+        Plots the coordinate structure if it's a 3D structure
+
+        :returns:   The scene instance
+        :rtype:     render3D.SceneList
+        """
+        scene = render3D.SceneList()
+
+        ax = scene.append_ax()
+
+        coordinates = numpy.c_[self.x, self.y, self.z].T
+
+        ax.add_unstructured_mesh(
+            coordinates=coordinates,
+            scalar_coloring=None
+        )
+
+        return scene
+
+
+a = CartesianCoordinates.generate_from_square(length=10, n_points=10)
+# a.shift_coordinates([10, 0])
+
+a.shift_coordinates([3, 1, 1])
+
+a.plot().show()
 
 
 def vector_cyl2cart(cylindrical_vector: numpy.ndarray, vector_position: CartesianCoordinates | CylindricalCoordinates) -> tuple:
